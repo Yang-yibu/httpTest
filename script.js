81f7d34ab275d5503dd05b7f3e24c0e0e0836981
Yang-yibu <18335834527@163.com>
2018-12-09 17:46:51 +0800

    3-4 缓存头Cache-Control的含义和使用

---
+++ script.js
@@ -0,0 +1,5 @@
+var a = 1;
+var log = 'scriptJs 2: ' + a;
+console.log(log);
+document.body.appendChild(document.createTextNode(log))
+// console.log(2);
--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const fs = require('fs')
 http.createServer(function (req, res) {
   console.log('request come: ', req.url);
 
-  const html = fs.readFileSync('test.html', 'utf8');
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
-  res.end(html);
+  if (req.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf8');
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.end(html);
+  }
+  if (req.url === '/script.js') {
+    const js = fs.readFileSync('script.js', 'utf8');
+    res.writeHeader(200, {
+      'Content-Type': 'text/javascript',
+      // 当 max-age 设置过小时，可能资源可能不缓存
+      'Cache-Control': 'max-age=200'
+    })
+    res.end(js)
+  }
 
 }).listen(8888)
 console.log('server running: 8888');
