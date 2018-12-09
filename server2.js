40dca2f2a41baee563bdc9f2a0a0b468af373fc4
Yang-yibu <18335834527@163.com>
2018-12-09 14:40:28 +0800

    3-2 CORS跨域请求的限制与解决: jsonp

--- server2.js
+++ server2.js
@@ -6,9 +6,16 @@ http.createServer(function (req, res) {
 
   res.writeHead(200, {
     // 'Access-Control-Allow-Origin': '*'
-    'Access-Control-Allow-Origin': 'http://127.0.0.1:8887'
+    // 发起请求的域名
+    // 'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
+    'Content-Type': 'application/JSON'
   })
 
-  res.end('{"data": 123}');
+  // res.end('{"data": 123}');
+  let resData = {
+    data: { key: 123 },
+    errno: 0
+  }
+  res.end('callbackFun('+ JSON.stringify(resData) +')')
 }).listen(8887)
 console.log('server running: 8887');
