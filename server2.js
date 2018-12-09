5e333d51284ba84a42ab65b4b729714586f2fd60
Yang-yibu <18335834527@163.com>
2018-12-09 14:43:45 +0800

    3-2 CORS跨域请求的限制与解决: 配置可访问的白名单

--- server2.js
+++ server2.js
@@ -7,14 +7,15 @@ http.createServer(function (req, res) {
   res.writeHead(200, {
     // 'Access-Control-Allow-Origin': '*'
     // 发起请求的域名
-    // 'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
+    // 允许多域：通过 req.host 等动态判断
+    'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
   })
 
-  // res.end('{"data": 123}');
-  let resData = {
-    data: { key: 123 },
-    errno: 0
-  }
-  res.end('callbackFun('+ JSON.stringify(resData) +')')
+  res.end('{"data": 123}');
+  // let resData = {
+  //   data: { key: 123 },
+  //   errno: 0
+  // }
+  // res.end('callbackFun('+ JSON.stringify(resData) +')')
 }).listen(8887)
 console.log('server running: 8887');
